f7b4da4a43bb4b439875ba1537dbe412239e71d7
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2025-06-26 19:20:09 -0300

    feat: added new endpoint to send scheduled messages

--- scheduler-api/app/crud.py
+++ scheduler-api/app/crud.py
@@ -1,22 +1,38 @@
 from sqlalchemy.orm import Session
+from app.logger import logger
 from app.models import ScheduledMessage
 from app.schemas import MessageCreate
 
 
-def create_message(db: Session, msg: MessageCreate):
-    db_msg = ScheduledMessage(
-        content=msg.content,
-        scheduled_time=msg.scheduled_time
-    )
-    db.add(db_msg)
-    db.commit()
-    db.refresh(db_msg)
-    return db_msg
+def create_scheduled_message(db: Session, msg: MessageCreate):
+    try:
+        message = ScheduledMessage(
+            content=msg.content,
+            scheduled_time=msg.scheduled_time
+        )
+        db.add(message)
+        db.commit()
+        db.refresh(message)
+        return message
+    except Exception as e:
+        logger.info(f"Error: failed to create a message: {e}")
+        raise
 
 
-def get_message(db: Session, msg_id: int):
+def get_message_by_id(db: Session, msg_id: int):
     return db.query(ScheduledMessage).filter(ScheduledMessage.id == msg_id).first()
 
 
-def list_messages(db: Session):
+def get_all_messages(db: Session):
     return db.query(ScheduledMessage).order_by(ScheduledMessage.scheduled_time).all()
+
+
+def send_scheduled_message(db: Session, msg):
+    try:
+        msg.sent = True
+        db.commit()
+        db.refresh(msg)
+        logger.info(f"Message: \"{msg.content}\" was sent successfully!")
+    except Exception as e:
+        logger.error(f"Failed to send a message: {msg}, error: {e}")
+        raise e
--- scheduler-api/app/messages.py
+++ scheduler-api/app/messages.py
@@ -1,8 +1,9 @@
 from typing import List
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 
-from app.crud import create_message, list_messages, get_message
+from app.crud import create_scheduled_message, get_all_messages, get_message_by_id, send_scheduled_message
 from app.database import get_session
 from app.schemas import MessageCreate, MessageOut
 
@@ -10,18 +11,35 @@ router = APIRouter()
 
 
 @router.post('/schedule', response_model=MessageOut)
-def schedule_message(msg: MessageCreate, db: Session = Depends(get_session)):
-    return create_message(db, msg)
+def create_message(msg: MessageCreate, db: Session = Depends(get_session)):
+    return create_scheduled_message(db, msg)
+
+
+@router.get('/messages/{msg_id}', response_model=MessageOut)
+def get_message(msg_id: int, db: Session = Depends(get_session)):
+    msg = get_message_by_id(db, msg_id)
+    if not msg:
+        raise HTTPException(status_code=404, detail='Message not found')
+    return msg
 
 
 @router.get('/messages', response_model=List[MessageOut])
 def get_messages(db: Session = Depends(get_session)):
-    return list_messages(db)
+    return get_all_messages(db)
 
 
-@router.get('/messages/{msg_id}', response_model=MessageOut)
-def get_message_by_id(msg_id: int, db: Session = Depends(get_session)):
-    msg = get_message(db, msg_id)
+@router.put('/message/{msg_id}/send')
+def send_message(msg_id: int, db: Session = Depends(get_session)):
+    msg = get_message_by_id(db, msg_id)
     if not msg:
         raise HTTPException(status_code=404, detail='Message not found')
-    return msg
+    if msg.sent:
+        raise HTTPException(status_code=400, detail='Message already sent')
+
+    try:
+        send_scheduled_message(db, msg)
+    except:
+        raise HTTPException(status_code=500, detail='Failed to send message')
+    
+    return JSONResponse(status_code=200, content={ "detail": "Message sent successfully" })
+
